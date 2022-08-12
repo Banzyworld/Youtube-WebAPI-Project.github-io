3986a408b375f3a5d1b66c7f2059798e5f36dee7
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-08-12 03:16:27 +0100

    deleted anchor link

--- src/App.js
+++ src/App.js
@@ -9,18 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-<<<<<<< HEAD
-          Welcome to Group C Youtube API projects 
-=======
-          Group C Youtube API Project
->>>>>>> 1a74e8453c0d19f931a0bf8bb0aae6988ebd5852
-        </a>
+     
         <p>Just making dummy edits here to know if it commits</p>
       </header>
     </div>
