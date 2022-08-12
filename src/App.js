5ddc77e01593357d6b8a450207c709a917040de7
Alofun Segun <segunalofun@protonmail.com>
2022-08-12 01:58:20 +0100

    little description to the anchor tag

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Group C Youtube API Project
         </a>
       </header>
     </div>
