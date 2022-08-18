da137bb86a5e9a0578907301efb126f05fc27709
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-08-18 01:08:04 +0100

    imported header and sidebar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
+import Header from './Components/Header';
+
+import SideBar from './Components/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-     
-        <p>Just making dummy edits here to know if it commits</p>
-      </header>
-    </div>
+   <div>
+    {/* <Header /> */}
+    <SideBar />
+   </div>
   );
 }
 
