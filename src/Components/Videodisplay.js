e625eee3d365645a2b6112ab2bb792c0c512e7bb
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-22 12:36:22 +0100

    fetched response of items needed for youtube display and saved it so function names createVideoCards

--- src/Components/Videodisplay.js
+++ src/Components/Videodisplay.js
@@ -5,14 +5,16 @@ import { CircularProgress } from '@mui/material';
 import {DateTime} from 'luxon';
 import { Alert } from '@mui/material'
 
-
 const Videodisplay =  () => {
+    const [videoCards, setVideoCards] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
     useEffect(() => {
     fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=12&regionCode=US&key=AIzaSyCJ8IYdyJgbQqhUhoXl9gWNCRYJLJlUVqE")
     .then(response => {
-        return (response.json())
-    }).then((response)=>{
-        console.log(response)
+        console.log (response.data.items)
+        createVideoCards(response.data.items)
     })
     .catch(error => {
       console.log(error);
