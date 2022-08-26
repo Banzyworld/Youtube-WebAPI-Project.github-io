44f9e9baadf10285b248360f8ec375a78e9a92dd
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-08-26 13:54:39 +0100

    Linked the page using router, switch, route respectively

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import Sidebar from './Components/Sidebar';
 import './App.css';
 import Header from './Components/Header';
 import Videodisplay from './Components/Videodisplay';
+import SearchBarPage from './Components/SearchBarPage';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 function App() {
+
+
   return (
    <div className="App">
+      <Router>
       <Header/>
-       <div className="app__mainpage">
-          <Sidebar/>
-          <Videodisplay/>
-      </div>
+         <Switch>
+            <Route path='/search/:searchQuery'>
+               <div className="app__mainpage">
+                  <Sidebar />
+                  <SearchBarPage />
+               </div>
+            </Route>
+            <Route path='/'>
+               <div className="app__mainpage">
+                  <Sidebar />
+                  <Videodisplay />
+               </div>
+            </Route>
+         </Switch>
+      </Router>
    </div>
   );
 }
