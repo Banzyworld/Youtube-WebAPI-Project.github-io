481ff88e0cda42c9dfafde61bc2042c575b1da4c
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-29 16:50:16 +0100

    created props for channel display page on hitting search button

--- src/Components/ChannelSection.js
+++ src/Components/ChannelSection.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './ChannelSection.css';
+import { VscAccount } from "react-icons/vsc"
 
-const ChannelSection = () => {
+const ChannelSection = ({image, channel, subs, noOfVideos, description}) => {
   return (
-    <div>ChannelSection</div>
+    <div className='channelsection'>
+        <VscAccount size={32}
+         className='channelsection__logo'
+         alt={channel}
+         src={image}
+         />
+
+        <div className="channelsection__text">
+            <h4>{channel}</h4>
+            <p>{subs} subscribers • {noOfVideos} videos</p>
+            <p>{description}</p>
+        </div>
+    </div>
   )
 }
 
