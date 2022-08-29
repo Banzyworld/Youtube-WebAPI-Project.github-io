12cbe44ae30dc587866227a0ceccaef6d1977096
YhinkaDevOps <yinkabakare30@gmail.com>
2022-08-29 16:57:46 +0100

    added styling to videorow function component

--- src/Components/VideoRow.js
+++ src/Components/VideoRow.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import './VideoRow.css'
 
-const VideoRow = () => {
+const VideoRow = ({views, description, timestamp, channel, title, image}) => {
   return (
-    <div>VideoRow</div>
+    <div className='videorow'>
+        <img src={image} alt="" />
+        <div className="videorow__text">
+            <h3>{title}</h3>
+            <p className='videorow__headline'>
+            {channel} • {views} views • {timestamp}
+            </p>
+            <p className='videorow__description'>
+            {description}
+            </p>
+          </div>
+    </div>
   )
 }
 
