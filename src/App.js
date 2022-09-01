4341dd88b8823cdf5648d3b7614e43e0e3d99290
Banzyworld <102680772+Banzyworld@users.noreply.github.com>
2022-09-02 00:35:22 +0100

    removed BrowserRouter and Router import

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@ import Sidebar from './Components/Sidebar';
 import './App.css';
 import Header from './Components/Header';
 import Videodisplay from './Components/Videodisplay';
-import SearchBarPage from './Components/SearchBarPage';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import SearchBarPage from './Components/SearchBarPage';
+// import {Router, Routes, Route} from 'react-router-dom';
+// import {
+//  //   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
 
 
   return (
    <div className="App">
-      <Router>
-      <Header/>
-         <Routes>
-            <Route path='/search/:searchQuery'>
+      
+   <Header/>
+            
                <div className="app__mainpage">
+               
                   <Sidebar />
-                  <SearchBarPage />
-               </div>
-            </Route>
-            <Route path='/'>
-               <div className="app__mainpage">
-                  <Sidebar />
+                     
                   <Videodisplay />
+                
                </div>
-            </Route>
-         </Routes>
-      </Router>
+       
    </div>
   );
 }
